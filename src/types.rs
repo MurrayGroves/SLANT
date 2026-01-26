15cbb33a973b0bf3200f8ed9d20b3dc5f9be47fd
Murray Groves <github@murraygrov.es>
2026-01-26 13:58:49 +0000

    Add ability for packets to specify eager NodeID targets

--- src/types.rs
+++ src/types.rs
@@ -41,6 +41,10 @@ impl<A: kiddo::float::kdtree::Axis, const K: usize> Packet<A, K> for UnicastPack
         self.content
     }
 
+    fn eager_targets(&self) -> Option<Vec<NodeID>> {
+        Some(vec![self.target])
+    }
+
     fn targets(&self, target: &NodeData<A, K>) -> bool {
         target.id == self.target
     }
@@ -55,6 +59,10 @@ impl<A: kiddo::float::kdtree::Axis, const K: usize> Packet<A, K> for MulticastPa
         self.content
     }
 
+    fn eager_targets(&self) -> Option<Vec<NodeID>> {
+        None
+    }
+
     fn targets(&self, target: &NodeData<A, K>) -> bool {
         true
     }
@@ -62,6 +70,11 @@ impl<A: kiddo::float::kdtree::Axis, const K: usize> Packet<A, K> for MulticastPa
 
 pub trait Packet<A: kiddo::float::kdtree::Axis, const K: usize> {
     fn content(self) -> Box<[u8]>;
+
+    /// Returns a vec of all NodeIDs which could receive the packet if they are in range.
+    /// Means the transmitter can avoid having to lookup all nodes in range.
+    /// Return None if the targeting is dynamic.
+    fn eager_targets(&self) -> Option<Vec<NodeID>>;
 
     /// Whether a packet should be received by a given node
     fn targets(&self, target: &NodeData<A, K>) -> bool;
